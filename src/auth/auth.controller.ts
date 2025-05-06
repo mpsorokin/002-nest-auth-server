2e6b846037382bb38736248ca1095d405c314728
Aa <aa@example.com>
2025-05-06 13:53:36 +0300

    added prisma

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -31,7 +31,7 @@ export class AuthController {
 
 	@Post('logout')
 	@HttpCode(HttpStatus.OK)
-	logout(@Req() req: Request, @Res() res: Response) {
+	logout(@Req() req: Request, @Res({ passthrough: true }) res: Response) {
 		return this.authService.logout(req, res)
 	}
 }
