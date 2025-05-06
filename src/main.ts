f77ea36b74ff04502946147c4bebc80c579e246e
Aa <aa@example.com>
2025-05-06 11:54:47 +0300

    added prisma

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import { ValidationPipe } from '@nestjs/common'
 import { ConfigService } from '@nestjs/config'
 import { NestFactory } from '@nestjs/core'
 import * as cookieParser from 'cookie-parser'
+import * as session from 'express-session'
 import IORedis from 'ioredis'
 
 import { AppModule } from './app.module'
@@ -15,6 +16,21 @@ async function bootstrap() {
 	app.use(cookieParser(config.getOrThrow<string>('COOKIES_SECRET')))
 
 	app.useGlobalPipes(new ValidationPipe({ transform: true }))
+
+	app.use(
+		session({
+			secret: config.getOrThrow<string>('SESSION_SECRET'),
+			name: config.getOrThrow<string>('SESSION_NAME'),
+			resave: true,
+			saveUninitialized: false,
+			cookie: {
+				domain: config.getOrThrow<string>('SESSION_DOMAIN'),
+				maxAge: 30 * 24 * 60 * 60,
+				httpOnly: false,
+				secure: false
+			}
+		})
+	)
 
 	app.enableCors({
 		origin: config.getOrThrow<string>('ALLOWED_ORIGIN'),
