700c0b2c0d00f9935388bb495cdc3aa21cd9ed8b
Aa <aa@example.com>
2025-05-06 12:51:54 +0300

    added prisma

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,10 +1,19 @@
-import { Injectable } from '@nestjs/common'
+import { ConflictException, Injectable } from '@nestjs/common'
+
+import { UserService } from '../user/user.service'
 
 import { RegisterDto } from './dto/register.dto'
 
 @Injectable()
 export class AuthService {
-	async register(dto: RegisterDto) {}
+	constructor(private readonly userService: UserService) {}
+	async register(dto: RegisterDto) {
+		const isExists = await this.userService.findByEmail(dto.email)
+
+		if (isExists) {
+			throw new ConflictException('Email already exists')
+		}
+	}
 
 	async login() {}
 
