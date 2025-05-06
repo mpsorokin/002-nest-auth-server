87d0b7b8ff5d73d8b8975e803599ddf3b3a45b8c
Aa <aa@example.com>
2025-05-06 08:38:50 +0300

    initial

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,25 @@
+import { ValidationPipe } from '@nestjs/common'
+import { ConfigService } from '@nestjs/config'
 import { NestFactory } from '@nestjs/core'
+import * as cookieParser from 'cookie-parser'
 
 import { AppModule } from './app.module'
 
 async function bootstrap() {
 	const app = await NestFactory.create(AppModule)
+
+	const config = app.get(ConfigService)
+
+	app.use(cookieParser(config.getOrThrow<string>('COOKIES_SECRET')))
+
+	app.useGlobalPipes(new ValidationPipe({ transform: true }))
+
+	app.enableCors({
+		origin: config.getOrThrow<string>('ALLOWED_ORIGIN'),
+		credentials: true,
+		exposeHeaders: ['set-cookie']
+	})
+
 	await app.listen(process.env.PORT ?? 3071)
 }
 bootstrap()
