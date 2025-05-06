b04115471880273ff7f0fdd9b30c58c8ac87dc08
Aa <aa@example.com>
2025-05-06 17:12:35 +0300

    added provider types

--- src/auth/provider/provider.service.ts
+++ src/auth/provider/provider.service.ts
@@ -1,4 +1,21 @@
-import { Injectable } from '@nestjs/common'
+import { Inject, Injectable, OnModuleInit } from '@nestjs/common'
+
+import { ProviderOptionsSymbol, TypeOptions } from './provider.constants'
+import { BaseOauthService } from './services/base-oauth.service'
 
 @Injectable()
-export class ProviderService {}
+export class ProviderService implements OnModuleInit {
+	constructor(
+		@Inject(ProviderOptionsSymbol) private readonly options: TypeOptions
+	) {}
+
+	onModuleInit() {
+		for (const provider of this.options.services) {
+			provider.baseUrl = this.options.baseUrl
+		}
+	}
+
+	findByService(service: string): BaseOauthService | null {
+		return this.options.services.find(s => s.name === service) ?? null
+	}
+}
