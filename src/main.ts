96e4095ddd139f7dece1ffc03b503d9f6357f359
Aa <aa@example.com>
2025-05-06 11:37:17 +0300

    added prisma

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import { ValidationPipe } from '@nestjs/common'
 import { ConfigService } from '@nestjs/config'
 import { NestFactory } from '@nestjs/core'
 import * as cookieParser from 'cookie-parser'
+import IORedis from 'ioredis'
 
 import { AppModule } from './app.module'
 
@@ -9,6 +10,7 @@ async function bootstrap() {
 	const app = await NestFactory.create(AppModule)
 
 	const config = app.get(ConfigService)
+	const redis = new IORedis(config.getOrThrow<string>('REDIS_URI'))
 
 	app.use(cookieParser(config.getOrThrow<string>('COOKIES_SECRET')))
 
