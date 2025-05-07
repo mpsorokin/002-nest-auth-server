878a70de7e0b4a64e19cfc5e99a77000477dff46
Aa <aa@example.com>
2025-05-07 10:02:32 +0300

    added provider types

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,22 +1,34 @@
 import {
+	BadRequestException,
 	Body,
 	Controller,
+	Get,
 	HttpCode,
 	HttpStatus,
+	Param,
 	Post,
+	Query,
 	Req,
-	Res
+	Res,
+	UseGuards
 } from '@nestjs/common'
+import { ConfigService } from '@nestjs/config'
 import { Recaptcha } from '@nestlab/google-recaptcha'
 import { Request, Response } from 'express'
 
 import { AuthService } from './auth.service'
 import { LoginDto } from './dto/login.dto'
 import { RegisterDto } from './dto/register.dto'
+import { AuthProviderGuard } from './guards/provider.guard'
+import { ProviderService } from './provider/provider.service'
 
 @Controller('auth')
 export class AuthController {
-	constructor(private readonly authService: AuthService) {}
+	constructor(
+		private readonly authService: AuthService,
+		private readonly configService: ConfigService,
+		private readonly providerService: ProviderService
+	) {}
 
 	@Recaptcha()
 	@Post('register')
@@ -30,6 +42,29 @@ export class AuthController {
 	@HttpCode(HttpStatus.OK)
 	login(@Req() req: Request, @Body() dto: LoginDto) {
 		return this.authService.login(req, dto)
+	}
+
+	@Get('/oauth/callback/:provider')
+	@UseGuards(AuthProviderGuard)
+	async callback(
+		@Req() req: Request,
+		@Res({ passthrough: true }) res: Response,
+		@Param('provider') provider: string,
+		@Query('code') code: string
+	) {
+		if (!code) {
+			throw new BadRequestException()
+		}
+	}
+
+	@UseGuards(AuthProviderGuard)
+	@Get('/oauth/connect/:provider')
+	@HttpCode(HttpStatus.OK)
+	async connect(@Param('provider') provider: string) {
+		const providerInstance = this.providerService.findByService(provider)
+		return {
+			url: providerInstance?.getAuthUrl()
+		}
 	}
 
 	@Post('logout')
