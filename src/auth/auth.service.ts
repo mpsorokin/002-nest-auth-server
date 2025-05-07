25cbffb84f81d55d78e55dded3eb64adda2c1c8c
Aa <aa@example.com>
2025-05-07 09:51:14 +0300

    added provider types

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -10,16 +10,20 @@ import { verify } from 'argon2'
 import { Request, Response } from 'express'
 
 import { AuthMethod, User } from '../../generated/prisma'
+import { PrismaService } from '../prisma/prisma.service'
 import { UserService } from '../user/user.service'
 
 import { LoginDto } from './dto/login.dto'
 import { RegisterDto } from './dto/register.dto'
+import { ProviderService } from './provider/provider.service'
 
 @Injectable()
 export class AuthService {
 	constructor(
 		private readonly userService: UserService,
-		private readonly configService: ConfigService
+		private readonly configService: ConfigService,
+		private readonly providerService: ProviderService,
+		private readonly prismaService: PrismaService
 	) {}
 	async register(req: Request, dto: RegisterDto) {
 		const isExists = await this.userService.findByEmail(dto.email)
@@ -51,6 +55,55 @@ export class AuthService {
 
 		if (!isValidPassword) {
 			throw new UnauthorizedException('Incorrect password')
+		}
+
+		return this.saveSession(req, user)
+	}
+
+	async extractProfileFromCode(req: Request, provider: string, code: string) {
+		const providerInstance = this.providerService.findByService(provider)
+
+		const profile = await providerInstance?.findUserByCode(code)
+
+		if (!profile) {
+			throw new NotFoundException('User not found')
+		}
+
+		const account = await this.prismaService.account.findFirst({
+			where: {
+				id: profile.id,
+				provider: profile.provider
+			}
+		})
+
+		let user = account?.userId
+			? await this.userService.findById(account.userId)
+			: null
+
+		if (user) {
+			return this.saveSession(req, user)
+		}
+
+		user = await this.userService.create(
+			profile.email,
+			'',
+			profile.name,
+			profile.picture,
+			AuthMethod[profile.provider.toUpperCase()],
+			true
+		)
+
+		if (!account) {
+			await this.prismaService.account.create({
+				data: {
+					userId: user.id,
+					type: 'oauth',
+					provider: profile.provider,
+					accessToken: profile.access_token,
+					refreshToken: profile.refresh_token,
+					expiresAt: profile.expires_at || 0
+				}
+			})
 		}
 
 		return this.saveSession(req, user)
