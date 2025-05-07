966e3206a83ea4737f87a325be745f00779efc9f
Aa <aa@example.com>
2025-05-07 09:15:10 +0300

    added provider types

--- src/auth/provider/provider.module.ts
+++ src/auth/provider/provider.module.ts
@@ -1,8 +1,41 @@
-import { Module } from '@nestjs/common'
+import { DynamicModule, Module } from '@nestjs/common'
 
+import {
+	ProviderOptionsSymbol,
+	TypeAsyncOptions,
+	TypeOptions
+} from './provider.constants'
 import { ProviderService } from './provider.service'
 
-@Module({
-	providers: [ProviderService]
-})
-export class ProviderModule {}
+@Module({})
+export class ProviderModule {
+	public static register(options: TypeOptions): DynamicModule {
+		return {
+			module: ProviderModule,
+			providers: [
+				{
+					useValue: options.services,
+					provide: ProviderOptionsSymbol
+				},
+				ProviderService
+			],
+			exports: [ProviderService]
+		}
+	}
+
+	public static registerAsync(options: TypeAsyncOptions): DynamicModule {
+		return {
+			module: ProviderModule,
+			imports: options.imports,
+			providers: [
+				{
+					useFactory: options.useFactory,
+					provide: ProviderOptionsSymbol,
+					inject: options.inject
+				},
+				ProviderService
+			],
+			exports: [ProviderService]
+		}
+	}
+}
